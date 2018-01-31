8feb57c35dd4bd2456777db9393750c26d7d728b
Fatechosen <idreamyc@gmail.com>
2018-01-31 13:08:08 -0700

    Debugging

--- project/unit1/Proj1.py
+++ project/unit1/Proj1.py
@@ -3,7 +3,7 @@
 
 import xml.etree.ElementTree as ET
 import sqlite3
-import cvs
+import csv
 import sys
 import os
 
@@ -14,9 +14,9 @@ cur = conn.cursor()
 db = open("Proj1.csv","w")
 
 def createTable():
-     cur.execute(
-		"DROP TABLE Node; DROP TABLE Way; DROP TABLE Nodetag; DROP TABLE Waytag;DROP TABLE Waypoints;"
-		)
+     #cur.execute(
+		#"DROP TABLE Node; DROP TABLE Way; DROP TABLE Nodetag; DROP TABLE Waytag;DROP TABLE Waypoints;"
+		#)
 	#generate tables
      cur.execute(
 		"CREATE TABLE Node ( id INTEGER, lat FLOAT, lon FLOAT, PRIMARY KEY (id) );"
@@ -39,48 +39,48 @@ def createTable():
      conn.commit()
 
 def formatData():
-    #db
+     #db
+     
+     # Read the XML file
+     tree = ET.parse('edmonton.osm')
+     root = tree.getroot()
+	 
+     # go through all the information
+     for child in root:
+          print (child.tag)
+          print (child.attrib)
+		 
+          # Insert nodes and nodetag
+          if (child.tag == 'node'):
+               db.write("INSERT INTO Node VALUES (" + str(child.attrib['id']) +','
+	                                            + str(child.attrib['lat']) +','+ str(child.attrib['lon']) +')'  )
+     
+          for tag in child.iter('tag'):
+               db.write("INSERT INTO Nodetag VALUES (" + str(child.attrib['id']) +','
+	                                          + str(tag.attrib['k']) +','+ str(tag.attrib['v']) +")")
+		 
+	       # Insert ways and Waytag
+	       # Need to check: 
+	       # close DONE
+	       # if the nodeid in the current db TODO
+          if (child.tag == 'way'):
+               ord = 0
+               close = False	
+               for ref in child.iter('ref'):
+                    db.write("INSERT INTO Waypoint VALUES (" + str(child.attrib['id']) +','
+	                     + str(ord) +','+ str(ref) +')'  )
+                    ord += 1
+  
+               for tag in child.iter('tag'):
+                    if 'close' in tag.attrib['v'] or 'Close' in tag.attrib['v']:
+                         close = True
+                         db.write("INSERT INTO Waytag VALUES (" + str(child.attrib['id']) +','
+			          + str(tag.attrib['k']) +','+ str(tag.attrib['v']) +")")
 
-    # Read the XML file
-    tree = ET.parse('edmonton.osm')
-    root = tree.getroot()
-	
-	# go through all the information
-	for child in root:
-		print child.tag
-		print child.attrib
-		
-		# Insert nodes and nodetag
-		if (child.tag == 'node'):
-			db.write("INSERT INTO Node VALUES (" + str(child.attrib['id'] +','
-					+ str(child.attrib['lat']) +','+ str(child.attrib['lon']) +')'  )
+               db.write("INSERT INTO Way VALUES (" + str(child.attrib['id']) +','+ str(close)  +')')
 
-			for tag in child.iter('tag'):
-				db.write("INSERT INTO Nodetag VALUES (" + str(child.attrib['id'] +','
-						+ str(tag.attrib['k']) +','+ str(tag.attrib['v']) +")")
-		
-		# Insert ways and Waytag
-		# Need to check: 
-		# close DONE
-		# if the nodeid in the current db TODO
-		if (child.tag == 'way'):
-			ord = 0
-			close = False	
-			for ref in child.iter('ref'):
-				db.write("INSERT INTO Waypoint VALUES (" + str(child.attrib['id'] +','
-						+ str(ord) +','+ str(ref) +')'  )
-				ord += 1
-
-			for tag in child.iter('tag'):
-				if 'close' in tag.attrib['v'] or 'Close' in tag.attrib['v']:
-					close = True
-				db.write("INSERT INTO Waytag VALUES (" + str(child.attrib['id'] +','
-						+ str(tag.attrib['k']) +','+ str(tag.attrib['v']) +")")
-
-			db.write("INSERT INTO Way VALUES (" + str(child.attrib['id']) +','+ str(close)  +')')
-
-
-				
+     
+		                
     
 
 

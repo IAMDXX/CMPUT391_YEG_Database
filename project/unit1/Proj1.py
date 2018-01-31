9cea0190803159ebdbc25e83e38f2928c76500d7
Fatechosen <idreamyc@gmail.com>
2018-01-31 16:43:32 -0700

    Update Insertion

--- project/unit1/Proj1.py
+++ project/unit1/Proj1.py
@@ -17,8 +17,9 @@ wp = open("wp.csv","w")
 nt = open("nt.csv","w")
 wt = open("wt.csv","w")
 
+nodes = []
+
 def createTable():
-	
     #drop previous exist tables
     cur.execute( "DROP TABLE IF EXISTS Node ;")
     cur.execute( "DROP TABLE IF EXISTS Way ;")
@@ -50,56 +51,68 @@ def formatData():
      #db
      
      # Read the XML file
-     tree = ET.parse('edmonton.osm')
-     root = tree.getroot()
+    tree = ET.parse('edmonton.osm')
+    root = tree.getroot()
 	 
-     # go through all the information
-     for child in root:
-          print (child.tag)
-          print (child.attrib)
-		 
-          # Insert nodes and nodetag
-          if (child.tag == 'node'):
-               nf.write(str(child.attrib['id']) +','+ str(child.attrib['lat']) +','+ str(child.attrib['lon']) )
-     
-          for tag in child.iter('tag'):
-               nt.write(str(child.attrib['id']) +','+ str(tag.attrib['k']) +','+ str(tag.attrib['v']) )
-		 
-	       # Insert ways and Waytag
-	       # Need to check: 
-	       # close DONE
-	       # if the nodeid in the current db TODO
-          if (child.tag == 'way'):
-               ord = 0
-               close = False	
-               for ref in child.iter('ref'):
-                    wp.write(str(child.attrib['id']) +','+ str(ord) +','+ str(ref) )
-                    ord += 1
-  
-               for tag in child.iter('tag'):
-                    if 'close' in tag.attrib['v'] or 'Close' in tag.attrib['v']:
-                         close = True
-                         wt.write(str(child.attrib['id']) +','+ str(tag.attrib['k']) +','+ str(tag.attrib['v']) )
+    # go through all the information
+    for node in root.iter('node'): 
+	# Insert nodes and nodetag
+        nf.write(str(node.attrib['id']) +','+ str(node.attrib['lat']) +','+ str(node.attrib['lon']) )
+        if (node.attrib['id'] not in nodes):
+            nodes.append(node.attrib['id'])
+   
+        for tag in node.iter('tag'):
+            nt.write(str(node.attrib['id']) +','+ str(node.attrib['k']) +','+ str(node.attrib['v']) )
+		
+    # Insert ways and Waytag
+    # Need to check: 
+    # close DONE
+    # if the nodeid in the current db TODO
+    for way in root.iter('way'):
+        close = False
+        discard = False
+        way_pts = []
+        for ref in way.iter('nd'):
+            if (ref.attrib['ref'] in nodes):
+                way_pts.append(ref.attrib['ref'])
+            else:
+                discard = True
+                break
+        if discard == True:
+            continue
+	    
+        ord = 0
+        for way_p in way_pts:
+            wp.write(str(way.attrib['id']) +','+ str(ord) +','+ str(way_p) )
+            ord += 1
+	    
+        if way_pts[0] == way_pts[-1]:
+            close = True
 
-               wf.write(str(child.attrib['id']) +','+ str(close))
+        for tag in way.iter('tag'):
+            if 'close' in tag.attrib['v'] or 'Close' in tag.attrib['v']:
+                close = True
+            wt.write(str(way.attrib['id']) +','+ str(tag.attrib['k']) +','+ str(tag.attrib['v']) )
 
-          nf.close()
-          wf.close()
-          nt.close()
-          wt.close()
-          wp.close()
-		                
+        wf.write(str(way.attrib['id']) +','+ str(close))
+
+    nf.close()
+    wf.close()
+    nt.close()
+    wt.close()
+    wp.close()
+			       
     
 
 
 def main():
     
-     # Create the Database Tables
-     createTable()
+    # Create the Database Tables
+    createTable()
     
-     # Load XML and insert the data
-     formatData()
+    # Load XML and insert the data
+    formatData()
     
     
-if __name__ == "__main__":
-     main()
+if __name__ == "__main__":    
+    main()

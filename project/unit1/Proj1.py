94be79601e31431f1411bb835d31af8934a2fe66
IAMDXX <yiting5@ualberta.ca>
2018-02-01 17:53:05 -0700

    Update Proj1.py

--- project/unit1/Proj1.py
+++ project/unit1/Proj1.py
@@ -1,26 +1,142 @@
 # Cmput391 Project 1
 # Team 20
+# Cmput391 Project 1
+# Team 20
 
+import csv
 import xml.etree.ElementTree as ET
 import sqlite3
-import csv
-import sys
-import os
+
+#sqlite_add = '/Users/DXX/Desktop/UACLASS/CMPUT391/proj1/proj1.db'
+#conn = sqlite3.connect(sqlite_add)
 
 
+parser = ET.iterparse('edmonton.osm')
 conn = sqlite3.connect(":memory:")
-cur = conn.cursor()
+cur = conn.cursor()  
+nodes = dict()
 
-nodes = []
+def formatData():
+    #db
+
+    # Read the XML file
+
+    #root = tree.getroot()
+
+    # go through all the information
+    with open('nf.csv', 'wb') as csvfile1, open('nt.csv', 'wb') as csvfile2, open('wp.csv', 'wb') as csvfile3, open('wt.csv', 'wb') as csvfile4, open('wf.csv', 'wb') as csvfile5:
+        #fieldnames = ['id', 'lat', 'lon']
+        writer1 = csv.writer(csvfile1)
+        #fieldnames = ['id', 'k', 'v']
+        writer2 = csv.writer(csvfile2)   
+        
+        f3 = ['id','ord','nodeid']
+        writer3 = csv.DictWriter(csvfile3,fieldnames = f3)
+        writer3.writeheader()
+        
+        f4 = ['id','k','v']
+        writer4 = csv.DictWriter(csvfile4,fieldnames = f4)
+        writer4.writeheader()
+        
+        f5 = ['id','close']
+        writer5 = csv.DictWriter(csvfile5,fieldnames = f5)
+        writer5.writeheader()        
+               
+        for event, element in parser:
+            # Insert nodes and nodetag
+            if element.tag == 'node':
+                writer1.writerow( [str(element.attrib['id']), str(element.attrib['lat']) ,str(element.attrib['lon'])] )
+                nodes[element.attrib['id']] = 0
+            #for tag in element.iter('tag'):
+                #writer2.writerow( [str(element.attrib['id']), tag.attrib['k'].encode('utf-8'), tag.attrib['v'].encode('utf-8')] )                
+
+                element.clear()            
+                
+            # Insert ways and Waytag
+            # Need to check: 
+            # close DONE
+            # if the nodeid in the current db TODO    
+    #with open('wp.csv', 'wb') as csvfile3, open('wt.csv', 'wb') as csvfile4, open('wf.csv', 'wb') as csvfile5:
+         
+            if element.tag == 'way':
+                start = 0
+                end = 0
+                close = False
+                discard = False
+                way_pts = []
+                for ref in element.iter('nd'):
+                    
+                    if (ref.attrib['ref'] in nodes.keys()):
+                        way_pts.append(ref.attrib['ref'])
+                    else:
+                        discard = True
+                        break
+                #if discard == True:
+                    #continue
+        
+                ord = 0
+    
+                for way_p in way_pts:
+                    if ord == 0:
+                        start = way_p
+                    if ord == len(way_pts)-1:
+                        end = way_p
+                    writer3.writerow( {'id': str(element.attrib['id']), 'ord': str(ord), 'nodeid': str(way_p)} )
+                    ord += 1
+        
+                if start == end:
+                    close = True
+                    
+                for tag in element.iter('tag'):
+                    if 'close' in tag.attrib['v'] or 'Close' in tag.attrib['v']:
+                        close = True
+                    writer4.writerow({'id': str(element.attrib['id']), 'k': tag.attrib['k'].encode('utf-8'), 'v':tag.attrib['v'].encode('utf-8')} )
+                    
+                writer5.writerow({'id': str(element.attrib['id']), 'close': str(close)})
+                
+                element.clear()
+    
+            
+def impCSV():
+    print("DONE")
+    with open('nf.csv','rb') as fin: 
+        dr = csv.reader(fin)
+        to_db = [(i[0], i[1], i[2]) for i in dr]
+    cur.executemany("INSERT INTO Node (id, lat, lon) VALUES (?, ?, ?);", to_db)
+    
+    with open('wf.csv','rb') as fin: 
+        dr = csv.reader(fin)
+        to_db = [(i[0], i[1]) for i in dr]
+    cur.executemany("INSERT INTO Way (id, closed) VALUES (?, ?);", to_db)
+    conn.commit()
+    
+    with open('nt.csv','rb') as fin: 
+        dr = csv.reader(fin)
+        to_db = [(i[0], str(i[1]), str(i[2])) for i in dr]
+    cur.executemany("INSERT INTO Nodetag (id, k, v) VALUES (?, ?, ?);", to_db)
+    
+    with open('wt.csv','rb') as fin: 
+        dr = csv.DictReader(fin)
+        to_db = [(i[0], str(i[1]), str(i[2])) for i in dr]
+    cur.executemany("INSERT INTO Waytag (id, k, v) VALUES (?, ?, ?);", to_db)
+    
+    with open('wp.csv','rb') as fin: 
+        dr = csv.reader(fin)
+        to_db = [(i[0], i[1], str(i[2])) for i in dr]
+    cur.executemany("INSERT INTO Waypoint (wayid, ordinal, nodeid) VALUES (?, ?, ?);", to_db)    
+    
+    conn.commit()
+    conn.close()   
 
 def createTable():
+    
     #drop previous exist tables
     cur.execute( "DROP TABLE IF EXISTS Node ;")
     cur.execute( "DROP TABLE IF EXISTS Way ;")
     cur.execute( "DROP TABLE IF EXISTS Waypoint ;")
     cur.execute( "DROP TABLE IF EXISTS Nodetag ;")
     cur.execute( "DROP TABLE IF EXISTS Waytag ;")
-
+            
     #generate tables
     cur.execute(
          "CREATE TABLE Node ( id INTEGER, lat FLOAT, lon FLOAT, PRIMARY KEY (id) );"
@@ -29,7 +145,7 @@ def createTable():
         "CREATE TABLE Way ( id INTEGER PRIMARY KEY, closed BOOLEAN );"
     )
     cur.execute(
-         "CREATE TABLE Waypoint ( wayid INTEGER, ordinal INTEGER, nodeid INTEGER, FOREIGN KEY (wayid) REFERENCES Way(id), FOREIGN KEY (nodeid) REFERENCES Node(id), CHECK (ordinal>=0 AND ordinal <= rowid ) );"
+         "CREATE TABLE Waypoint ( wayid INTEGER, ordinal INTEGER, nodeid INTEGER, FOREIGN KEY (wayid) REFERENCES Way(id), FOREIGN KEY (nodeid) REFERENCES Node(id), CHECK (ordinal>=0 AND ordinal < rowid ) );"
     )    
     cur.execute(
          "CREATE TABLE Nodetag ( id INTEGER, k TEXT, v TEXT, FOREIGN KEY (id) REFERENCES Node(id) );"
@@ -41,107 +157,19 @@ def createTable():
 
     conn.commit()
 
-def formatData():
-    nf = open("nf.csv","w")
-    wf = open("wf.csv","w")
-    wp = open("wp.csv","w")
-    nt = open("nt.csv","w")
-    wt = open("wt.csv","w")
-     
-     # Read the XML file
-    tree = ET.parse('edmonton.osm')
-    root = tree.getroot()
-	 
-    # go through all the information
-    for node in root.iter('node'): 
-	# Insert nodes and nodetag
-        nf.write(str(node.attrib['id']) +','+ str(node.attrib['lat']) +','+ str(node.attrib['lon']) )
-        if (node.attrib['id'] not in nodes):
-            nodes.append(node.attrib['id'])
-   
-        for tag in node.iter('tag'):
-            nt.write(str(node.attrib['id']) +','+ str(tag.attrib['k']) +','+ str(tag.attrib['v']) )
-		
-    # Insert ways and Waytag
-    # Need to check: 
-    # close DONE
-    # if the nodeid in the current db TODO
-    for way in root.iter('way'):
-        close = False
-        discard = False
-        way_pts = []
-        for ref in way.iter('nd'):
-            if (ref.attrib['ref'] in nodes):
-                way_pts.append(ref.attrib['ref'])
-            else:
-                discard = True
-                break
-        if discard == True:
-            continue
-	    
-        ord = 0
-        for way_p in way_pts:
-            wp.write(str(way.attrib['id']) +','+ str(ord) +','+ str(way_p) )
-            ord += 1
-	    
-        if way_pts[0] == way_pts[-1]:
-            close = True
-
-        for tag in way.iter('tag'):
-            if 'close' in tag.attrib['v'] or 'Close' in tag.attrib['v']:
-                close = True
-            wt.write(str(way.attrib['id']) +','+ str(tag.attrib['k']) +','+ str(tag.attrib['v']) )
-
-        wf.write(str(way.attrib['id']) +','+ str(close))
-
-    nf.close()
-    wf.close()
-    nt.close()
-    wt.close()
-    wp.close()
-			       
-    
-def impCSV():
-    with open('nf.csv','rb') as fin: 
-        dr = csv.DictReader(fin)
-        to_db = [(i['id'], i['lat'], i['lon']) for i in dr]
-    cur.executemany("INSERT INTO Node (id, lat, lon) VALUES (?, ?, ?);", to_db)
-    
-    with open('wf.csv','rb') as fin: 
-        dr = csv.DictReader(fin)
-        to_db = [(i['id'], i['closed']) for i in dr]
-    cur.executemany("INSERT INTO way (id, closed) VALUES (?, ?);", to_db)
-    
-    with open('nt.csv','rb') as fin: 
-        dr = csv.DictReader(fin)
-        to_db = [(i['id'], i['k'], i['v']) for i in dr]
-    cur.executemany("INSERT INTO notetag (id, k, v) VALUES (?, ?, ?);", to_db)
-    
-    with open('wt.csv','rb') as fin: 
-        dr = csv.DictReader(fin)
-        to_db = [(i['id'], i['k'], i['v']) for i in dr]
-    cur.executemany("INSERT INTO waytag (id, k, v) VALUES (?, ?, ?);", to_db)
-    
-    with open('wp.csv','rb') as fin: 
-        dr = csv.DictReader(fin)
-        to_db = [(i['wayid'], i['ordinal'], i['nodeid']) for i in dr]
-    cur.executemany("INSERT INTO waypoint (wayid, ordinal, nodeid) VALUES (?, ?, ?);", to_db)    
-    
-    con.commit()
-    con.close()    
 
 
 def main():
-    
+
     # Create the Database Tables
     createTable()
-    
+
     # Load XML and insert the data
     formatData()
     
     # Load CSV into database
-    impCSV()
-    
-    
+    impCSV()    
+
+
 if __name__ == "__main__":    
     main()

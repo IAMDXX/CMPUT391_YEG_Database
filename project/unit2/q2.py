7b70de1d8d12c7bc25b17257fc6c4b33f0644c3b
Fatechosen <idreamyc@gmail.com>
2018-02-28 11:44:59 -0700

    Compiled Q2, still working on efficiency

--- project/unit2/q2.py
+++ project/unit2/q2.py
@@ -16,7 +16,7 @@ def nodeDist(x1,y1,x2,y2):
     deltaY = math.cos(x2)*math.sin(y2) - math.cos(x1)*math.sin(y1)
     deltaZ = math.sin(x2) - math.sin(x1)
     
-    C = sqrt(deltaX^2 + deltaY^2 + deltaZ^2)
+    C = math.sqrt(deltaX*deltaX + deltaY*deltaY + deltaZ*deltaZ)
     
     D = R * C
     
@@ -24,42 +24,40 @@ def nodeDist(x1,y1,x2,y2):
 
 def main(argv): 
     try:
-        con = sqlite3.connect(argv[0])
+        con = sqlite3.connect(argv[1])
     except:
         print("The database file doesn't exist! ")
         return
-    global con
     cur = con.cursor()  
     con.create_function("nDist", 4, nodeDist)
     k = []
     v = []
-    
+    nodes = []
     ans = -1
     
-    for i in argv:
-      k.append(i[0:i.index('=')])
-      v.append(i[i.index('=')+1:])
-      
-    cur.executemany("SELECT DISTINCT id FROM nodetag WHERE k = %s AND v = %s", (k, v))
-    
-    nodes = cur.fetchall()
-    
-    for i in range(len(nodes)-1):
-      cur.execute ("SELECT lat, lon FROM node WHERE node.id = '%d'" , nodes[i])
-      lat1, lon1 = cur.fetchall()
-      for j in range(i+1,len(nodes)):
-        cur.execute ("SELECT lat, lon FROM node WHERE node.id = '%d'" , nodes[j])
-        lat2, lon2 = cur.fetchall() 
+    for i in argv[2:]:
+        k.append(i[0:i.index('=')])
+        v.append(i[i.index('=')+1:])
+    for i in range(len(k)):
+        cur.execute("SELECT DISTINCT id FROM nodetag WHERE k = ? AND v = ?", (k[i], v[i]))
+        nodes.append(cur.fetchall())
+        nodes = nodes[0]
+        print(nodes)
         
-        
-    
-        cur.execute("select nDist(?, ?, ?, ?)", (lat1, lon1, lat2, lon2))
-        
-        if cur.fetchone()[0] > ans:
-          ans = cur.fetchone()[0]
+    for i in range(len(nodes)):
+        cur.execute ("SELECT lat, lon FROM node WHERE node.id = ?" , (nodes[i][0],))
+        (lat1, lon1) = cur.fetchall()[0]
+        for j in range(i,len(nodes)):
+            cur.execute ("SELECT lat, lon FROM node WHERE node.id = ?" , (nodes[j][0],))
+            (lat2, lon2) = cur.fetchall()[0] 
+            cur.execute("select nDist(?, ?, ?, ?)", (lat1, lon1, lat2, lon2))
+            tmp = cur.fetchone()[0]
 
-    print("The number of the nodes:", len(nodes))
-    print("The longest distance between the nodes:", ans)
+            if tmp > ans:
+                ans = tmp
+
+    print("The number of the nodes: "+str(len(nodes)))
+    print("The longest distance between the nodes: "+str(ans)+'km')
 
 if __name__ == "__main__":
     main(sys.argv[0:])

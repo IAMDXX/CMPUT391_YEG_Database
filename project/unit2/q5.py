bcc0e9a1ec2fe5433af32724a3fedf6b29b85cf6
IAMDXX <yiting5@ualberta.ca>
2018-02-27 19:06:51 -0700

    Add files via upload

---
+++ project/unit2/q5.py
@@ -0,0 +1,26 @@
+#cmput 391 proj #2 team20
+#q5 
+import sys
+import csv
+import sqlite3
+
+db_file = sys.argv[1]
+tsv_file = sys.argv[2]
+
+#connet to database
+conn = sqlite3.connect(db_file)
+cur = conn.cursor()
+#turn on foreign key verification
+cur.execute('PRAGMA foreign_keys = ON;') 
+
+#read from tsv file
+with open(tsv_file) as tsvfile:
+    tsvreader = csv.reader(tsvfile, delimiter='\t')
+    for line in tsvreader:
+        attr = [int(line[0]), float(line[1]), float(line[2])]
+        cur.execute("INSERT INTO Node (id, lat, lon) VALUES (?,?,?)", attr)
+
+#commit and close database
+    conn.commit() 
+    conn.close()   
+ 

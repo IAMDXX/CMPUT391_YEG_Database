aeadbb7a82049f23cedb4fb0b0068c7f54101b47
IAMDXX <yiting5@ualberta.ca>
2018-03-22 18:06:14 -0600

    Update q2.py

--- project/unit3/q2.py
+++ project/unit3/q2.py
@@ -21,12 +21,12 @@ print ".read %s" % sql_file
 #create table
 print "CREATE TABLE IF NOT EXISTS areaMBR(id INTEGER PRIMARY KEY, minX FLOAT, maxX FLOAT, minY FLOAT, maxY FLOAT);"
 print '''
-        INSERT INTO areaMBR(id, minX, maxX, minY, maxY)
-        SELECT w.id, MIN(nc.x), MAX(nc.x), MIN(nc.y), MAX(nc.y) 
-        FROM way w, waypoint wp, nodeCartesian nc
-        WHERE w.closed = 1 AND w.id = wp.wayid AND wp.nodeid = nc.id 
-        GROUP BY w.id;
-        '''
+INSERT INTO areaMBR(id, minX, maxX, minY, maxY)
+SELECT w.id, MIN(nc.x), MAX(nc.x), MIN(nc.y), MAX(nc.y) 
+FROM way w, waypoint wp, nodeCartesian nc
+WHERE w.closed = 1 AND w.id = wp.wayid AND wp.nodeid = nc.id 
+GROUP BY w.id;
+'''
 #close db
 conn.commit()
 conn.close()

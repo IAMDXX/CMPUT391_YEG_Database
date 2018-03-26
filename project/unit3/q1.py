3359ee79890bde00ee9a95761f1f0f05709351d0
IAMDXX <yiting5@ualberta.ca>
2018-03-26 17:35:48 -0600

    Update q1.py

--- project/unit3/q1.py
+++ project/unit3/q1.py
@@ -10,16 +10,12 @@ cur = conn.cursor()
 #turn on foreign key verification
 cur.execute('PRAGMA foreign_keys = ON;')
 
-#Read from command shell
-sql_file = sys.argv[1]
-
 #Find min values for lat and lon
 origin = cur.execute("SELECT MIN(lat), MIN(lon) FROM node;")
 min_lat, min_lon = origin.fetchone()
 
 #stdout
 print ".open edmonton.db"
-print ".read %s" % sql_file
 print "CREATE TABLE IF NOT EXISTS nodeCartesian (id INTEGER PRIMARY KEY, x FLOAT, y FLOAT);"
 print "INSERT INTO nodeCartesian (id, x, y) SELECT id, ( (lat - %s ) * 111286), ( (lon - %s) * 67137 ) FROM node; " % (min_lat, min_lon)
                     

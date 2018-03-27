ae9c011111a492062ebb871e734159c924f4618f
Fatechosen <idreamyc@gmail.com>
2018-03-26 18:25:02 -0600

    q4 (not debugged yet)

---
+++ project/unit3/q4.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import time
+import random as rand
+import sqlite3
+import math
+
+
+def main(argv):
+     con = sqlite3.connect(argv[1])
+     cur = con.cursor()       
+     
+     l = argv[2]
+     k = argv[3]
+     
+     cur.execute ("select max(x),max(y) from nodeCartesian")
+     max_v = cur.fetchall()
+     max_x = max_v[0][0]
+     max_y = max_v[1][0]
+     
+     run_time = 0
+     
+     for execute in range(k):
+          h = l * rand.uniform(1, 10)
+          w = l * rand.uniform(1, 10)        
+          x1 = rand.uniform(0,max_x)
+          y1 = rand.uniform(0,max_y)
+          x2 = x1 + w
+          y2 = y1 + h
+          
+          # Check if there's a overlap
+          stmt = "SELECT COUNT(*) FROM areaMBR a WHERE (? BETWEEN a.minX and a.maxX AND ? BETWEEN a.minY AND maxY) OR (? BETWEEN a.minX and a.maxX AND ? BETWEEN a.minY AND maxY) \
+          OR (? BETWEEN a.minX and a.maxX AND ? BETWEEN a.minY AND maxY) OR (? BETWEEN a.minX and a.maxX AND ? BETWEEN a.minY AND maxY)"
+          
+          cur.execute(stmt, (x1,y1, x1,y2, x2,y1, x2,y2))
+          ol = cur.fetchone()
+          
+          if(ol[0] < 1):
+               continue
+          
+          # Begin to record time
+          stmt = "SELECT COUNT(*) FROM areaMBR a WHERE a.minX BETWEEN ? and ? AND a.maxX BETWEEN ? and ? AND a.minY BETWEEN ? and ? AND a.maxY BETWEEN ? and ?"
+          
+          start_time = time.time()
+          cur.execute(stmt, (x1,x2, x1,x2, y1,y2, y1,y2))
+          end_time = time.time()
+          
+          run_time += (start_time - end_time)
+          
+     print(str(k) + '\t' + str(l) + '\t' + str(run_tim/k))
+     
+if __name__ == "__main__":
+     main(sys.argv[0:])

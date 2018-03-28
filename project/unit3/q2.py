4d4697e8ecaa691ab9479201c7cf7e7597d1e320
IAMDXX <yiting5@ualberta.ca>
2018-03-27 19:46:13 -0600

    Update q2.py

--- project/unit3/q2.py
+++ project/unit3/q2.py
@@ -4,15 +4,12 @@ import sys
 import csv
 import sqlite3
 
-#Read sqlfile
-print ".open edmonton.db"
-
 #create table
-print "CREATE TABLE IF NOT EXISTS areaMBR(id INTEGER PRIMARY KEY, minX FLOAT, maxX FLOAT, minY FLOAT, maxY FLOAT);"
-print '''
+print("CREATE TABLE IF NOT EXISTS areaMBR(id INTEGER PRIMARY KEY, minX FLOAT, maxX FLOAT, minY FLOAT, maxY FLOAT);")
+print ('''
 INSERT INTO areaMBR(id, minX, maxX, minY, maxY)
 SELECT w.id, MIN(nc.x), MAX(nc.x), MIN(nc.y), MAX(nc.y) 
 FROM way w, waypoint wp, nodeCartesian nc
 WHERE w.closed = 1 AND w.id = wp.wayid AND wp.nodeid = nc.id 
 GROUP BY w.id;
-'''
+''')

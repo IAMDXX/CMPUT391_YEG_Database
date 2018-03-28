6ffea9388622a976875249e4154300a19b676bf7
IAMDXX <yiting5@ualberta.ca>
2018-03-27 19:45:32 -0600

    Update q1.py

--- project/unit3/q1.py
+++ project/unit3/q1.py
@@ -1,25 +1,7 @@
 #cmput 391 proj #3 team20
 #q1 
-import sys
-import csv
-import sqlite3
 
-#connet to database
-conn = sqlite3.connect('edmonton.db')
-cur = conn.cursor()
-#turn on foreign key verification
-cur.execute('PRAGMA foreign_keys = ON;')
-
-#Find min values for lat and lon
-origin = cur.execute("SELECT MIN(lat), MIN(lon) FROM node;")
-min_lat, min_lon = origin.fetchone()
-
-#stdout
-print ".open edmonton.db"
-print "CREATE TABLE IF NOT EXISTS nodeCartesian (id INTEGER PRIMARY KEY, x FLOAT, y FLOAT);"
-print "INSERT INTO nodeCartesian (id, x, y) SELECT id, ( (lat - %s ) * 111286), ( (lon - %s) * 67137 ) FROM node; " % (min_lat, min_lon)
-                    
-#close db
-conn.commit()
-conn.close()
+#Read sqlfile
+print( "CREATE TABLE IF NOT EXISTS nodeCartesian (id INTEGER PRIMARY KEY, x FLOAT, y FLOAT);" )
+print( "INSERT INTO nodeCartesian (id, x, y) SELECT id, ( (lat - lat_x) * 111286), ( (lon - lon_y) * 67137 ) FROM node, ( SELECT MIN(lat) AS lat_x, MIN(lon) AS lon_y FROM node ); " )
 
